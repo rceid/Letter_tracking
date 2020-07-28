dca6df5905fa479a004c76565d99235d35c2cfec
Raymond Eid <reid7@uchicago.edu>
2020-07-28 18:20:06 -0400

    finished video 11 of tutorial

--- django_proj/letter_tracking/urls.py
+++ django_proj/letter_tracking/urls.py
@@ -1,8 +1,18 @@
-#from django.contrib import admin
 from django.urls import path
+from .views import (LetterListView, 
+                LetterDetailView,
+                LetterCreateView,
+                LetterUpdateView,
+                LetterDeleteView,
+                UserLetterListView)
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='letter_tracking-home'),
+    path('', LetterListView.as_view(), name='letter_tracking-home'),
+    path('user/<str:username>/', UserLetterListView.as_view(), name='userletters'),
+    path('letter/int:<pk>/', LetterDetailView.as_view(), name='letter-detail'),
+    path('letter/new/', LetterCreateView.as_view(), name='letter-create'),
+    path('letter/int:<pk>/update/', LetterUpdateView.as_view(), name='letter-update'),
+    path('letter/int:<pk>/delete/', LetterDeleteView.as_view(), name='letter-delete'),
     path('about/', views.about, name='letter_tracking-about'),
 ]
--- django_proj/letter_tracking/views.py
+++ django_proj/letter_tracking/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
+from django.views.generic import (ListView,
+                                 DetailView,
+                                 CreateView,
+                                 UpdateView,
+                                 DeleteView)
 from .models import Letter
 
 
@@ -8,5 +15,58 @@ def home(request):
     }
     return render(request, 'letter_tracking/home.html', context)
 
+class LetterListView(ListView):
+    model = Letter
+    template_name = 'letter_tracking/home.html'
+    context_object_name = 'letters'
+    ordering = ['-date']
+    paginate_by = 5
+
+class UserLetterListView(ListView): #can adapt this to use for politicians. Video 11 pagination
+    model = Letter
+    template_name = 'letter_tracking/user_letters.html'
+    context_object_name = 'letters'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Letter.objects.filter(posted_by_id=user).order_by('-date_posted')
+
+class LetterDetailView(DetailView):
+    model = Letter
+
+class LetterCreateView(LoginRequiredMixin, CreateView):
+    model = Letter
+    fields = ['title', 'description', 'date']
+
+    def form_valid(self, form):
+        form.instance.posted_by = self.request.user
+        return super().form_valid(form)
+
+
+class LetterUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Letter
+    fields = ['title', 'description', 'date']
+
+    def form_valid(self, form):
+        form.instance.posted_by = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        letter = self.get_object()
+        if self.request.user == letter.posted_by:
+            return True
+        return False
+
+class LetterDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Letter
+    success_url = '/'
+
+    def test_func(self):
+        letter = self.get_object()
+        if self.request.user == letter.posted_by:
+            return True
+        return False
+
 def about(request):
-    return render(request, 'letter_tracking/about.html', {'title': '#Quedusallife'})
+    return render(request, 'letter_tracking/about.html', {'title': 'About'})
