5d07e856f7695fb6c9e44ffd575f1f5dcad55d7b
Raymond Eid <reid7@uchicago.edu>
2020-08-25 18:50:16 -0500

    safety

--- django_proj/letter_tracking/models.py
+++ django_proj/letter_tracking/models.py
@@ -186,11 +186,11 @@ class Letter(models.Model):
                                     )
     mención_directa_a_MX = models.CharField(max_length=3,
                                     choices=Dummy.choices,
-                                    verbose_name=_('Was Mexico directly mentioned?')
-                                    )
+                                    verbose_name=_('Was Mexico directly mentioned?'))
     destinatario = MultiSelectField(choices=[('', 'N/a')] + list(zip_choices(list(map(lambda obj: obj.recipient_name, Recipient.objects.all())))),
                                 default='None',
-                                max_length=100
+                                max_length=100,
+                                help_text="<em>if 'Other', please specify in the text box below</em>"
                                 )
     other_destinatario_comments = models.TextField()
 
